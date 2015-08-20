af81b3879d317b72724bc5b32a548bef0754afd9
Danilo Velozo <velozo.dan@gmail.com>
2015-08-20 09:46:44 -0300

    Page 191

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   root 'store#index', as: 'store'
-  
-  get 'store/index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 end
