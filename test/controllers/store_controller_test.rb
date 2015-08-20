ad8457f7a949fac4b59c05342ca77665504f69a0
Danilo Velozo <velozo.dan@gmail.com>
2015-08-20 00:42:57 -0300

    page 176

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
-
+# teste store_controller
 class StoreControllerTest < ActionController::TestCase
   test 'should get index' do
     get :index
     assert_response :success
   end
 
+  test 'markup need for store.js.coffe is in place' do
+    get :index
+    assert_select '.store .entry > img', 3
+    assert_select '.entry input[type=submit]', 3
+  end
 end
