7bc9f244672fa8c46543af5dcc4a4c8d53a7a44c
Danilo Velozo <velozo.dan@gmail.com>
2015-08-24 16:15:00 -0300

    finalizando depot, page 261

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  get 'admin', to: 'admin#index'
+  controller :sessions do
+    get 'login', as: :new
+    post 'login', as: :create
+    delete 'logout', as: :destroy
+  end
+
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index', as: 'store'
 
+  get 'store/index'
+  root 'store#index', as: 'store'
   resources :products do
     get :who_bought, on: :member
   end
